274bde3f71d35d3bf15a4204c2e17c3f04851dfb
Kermitts <91491739+Kermitts@users.noreply.github.com>
2022-04-27 18:55:26 +0200

    Update script.ps1

--- Fase3/script.ps1
+++ Fase3/script.ps1
@@ -1,1 +1,9 @@
-
+cmdlet Set-Location C:\
+New-Item Directorio_Princpipal
+cmdlet Set-Location Directorio_Principal
+New-Item Bachillerato
+New-Item Conserjeria
+New-Item Direccion
+New-Item Informatica
+cmdlet Set-Location C:\
+New-SMBShare –Name SOFTWARE –Path "C:\Directorio_Principal" –FullAccess "Domain Admins" –FullAccess "Bachillerato" –FullAccess "Direccion" –FullAccess "Informatica" –FullAccess "Conserjeria" -ReadAccess "Users"
